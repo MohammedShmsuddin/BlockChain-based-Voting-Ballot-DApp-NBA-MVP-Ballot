f9ace120d3f83edf5bbff5c1a81bf4a48e4c39e5
TAKTUK <taktukta@buffalo.edu>
2019-09-30 21:47:04 -0400

    Update dummy.sol

--- dummy.sol
+++ dummy.sol
@@ -30,14 +30,13 @@ contract NBA_MVP_Ballot {
     
     
 
-    Player[5] player; 
     uint candidateNumber = 0 ;   // to keep track of how many candidates there are
     uint iter= 0;
-
+    
     mapping(uint => Player)candidateRegister;  // 
     mapping(address => voter) voterRegister;
     mapping(address => uint) votePreference;  // for preference.
-
+    mapping(uint => uint) candidatePoints;  // i = 0... 4     =>   playerID1 , playerID2, playerID3 ,... playerID5
 
 // ------------------------------------------------------------------------------------------------------
 
@@ -58,7 +57,7 @@ function registerCandidate(uint _playerId) public returns (uint) {
     }
         candidateRegister[_playerId] = Player(_playerId, 0, true) ;  //registerCandidate("LBJ")  ...  source: https://solidity.readthedocs.io/en/v0.4.24/types.html
         candidateNumber += 1;  // now that we have a candidate registered , we can increase the counter
-        player[iter] = candidateRegister[_playerId];  // added to player 
+        candidatePoints[iter] = _playerId;  // pair each number 0...4 with playerID
         iter += 1;
         return _playerId;
 }
@@ -68,13 +67,6 @@ function unRegisterCandidate(uint _playerId) public  {
         candidateRegister[_playerId] = Player(_playerId, 0, false) ;  //registerCandidate("LBJ")  ...  source: https://solidity.readthedocs.io/en/v0.4.24/types.html
         candidateNumber -= 1;
         
-        
-        for(uint i=0; i < candidateNumber;i++){
-            if(candidateRegister[i].playerId == _playerId){
-                player[i] = Player(0,0,false);  // if player exists in player array.. remove it somehow
-            }
-            
-        }
 }
 
 // to unregister such voter, we will locate their voter object and make their instance variable isRegistered to false
@@ -96,9 +88,9 @@ function unRegisterVoter(address _voterAddress) public {
 // user will enter playerId of their top preferences
 // the candidates will have their points updated accoridng to the voter's preferences
 function votePlayer(address _voterAddress , uint pref1 , uint pref2, uint pref3, uint pref4, uint pref5) public {
-    if(voterRegister[_voterAddress].isRegistered && !(voterRegister[_voterAddress].hasVoted)) {  // verifying that voter is registered
+    if(voterRegister[_voterAddress].isRegistered && !(voterRegister[_voterAddress].hasVoted)  ) {  // verifying that voter is registered
      candidateRegister[pref1].points += 10;                                                     // also that the user hasn't already voted!
-     candidateRegister[pref2].points += 7;
+     candidateRegister[pref2].points += 7;          //also make sure all the candidates are registered
      candidateRegister[pref3].points += 5;
      candidateRegister[pref4].points += 3;
      candidateRegister[pref5].points += 1;
@@ -109,7 +101,9 @@ function votePlayer(address _voterAddress , uint pref1 , uint pref2, uint pref3,
     
 // this is used to collect all the points. We do so by accessing the playerID of the hashmap to get value -> points
 function tallyPoints (uint _playerId) public view returns(uint _points){
+    if(candidateRegister[_playerId].isRegistered){
   _points = candidateRegister[_playerId].points;
+    }
  return _points;
 }
 
@@ -117,11 +111,20 @@ function tallyPoints (uint _playerId) public view returns(uint _points){
   function winningProposal() public view returns (uint) {
         uint256 mostPoints = 0;
         uint winner ; // playerID of player with most points 
-        for (uint i = 0; i < player.length; i++)
-            if (player[i].points > mostPoints) {
-                mostPoints = player[i].points;
-                winner = player[i].playerId;
+        for (uint i = 0; i < candidateNumber; i++){
+            if (candidateRegister[candidatePoints[i]].points > mostPoints) { // candidate[i] gives u the ID of candidate
+                mostPoints = candidateRegister[candidatePoints[i]].points ; // each int .. from 0 to 4 is paired with 
+                                                                                //value playerID
+                winner = candidatePoints[i];
+          
             }
+ 
+            
+            
+        }
+            
             return winner;
     }
+    
+    
   }
