42df3c7a707355f0164ab9e32d1c0dafcdd4f0f7
taktuk <taktukta@buffalo.edu>
2019-10-25 07:01:35 -0400

    deleted main.js -- not needed anymore

--- ballot/ballot-app/src/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('#gifs-rows').hover(function() {
-      $(this).attr('src','/home/taktuk/Desktop/ballot/ballot-app/src/images/george.png');
-    },function() {
-      $(this).attr('src','/home/taktuk/Desktop/ballot/ballot-app/src/images/pg.gif');
-    }
-});
-// this doesnt work
