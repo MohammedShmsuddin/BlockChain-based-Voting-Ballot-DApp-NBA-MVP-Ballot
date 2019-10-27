3e45a0af81dc70e52aa1b17648d0f80d68b9d0b7
“[Mohammed <mohammedsamsuddin@Mohammeds-MacBook-Pro.local>
2019-10-27 11:45:10 -0400

    changes made to index.html and app.js. vote.html is avoided. index.html is being used for whole DApp

--- ballot/ballot-app/src/js/app.js
+++ ballot/ballot-app/src/js/app.js
@@ -1,7 +1,11 @@
-kApp = {
+ App = {
+
   web3Provider: null,
   contracts: {},
+  player: new Array(),
   url: 'http://127.0.0.1:7545',
+
+
   chairPerson:null,
   currentAccount:null,
   
@@ -12,7 +16,9 @@ kApp = {
      return App.initWeb3();
    },
   
-  
+  /**
+   * 
+   */
    initWeb3: function() {
         // Is there is an injected web3 instance?
     if (typeof web3 !== 'undefined') {
@@ -29,6 +35,11 @@ kApp = {
     return App.initContract();
   },
 
+
+
+  /**
+   * 
+   */
   initContract: function() {
       $.getJSON('NBA_MVP_Ballot.json', function(data) {
     // Get the necessary contract artifact file and instantiate it with truffle-contract
@@ -44,52 +55,164 @@ kApp = {
   },
 
 
+
+
+  /**
+   * 
+   */
   bindEvents: function() {    // none of these functions are working at the moments
-    $(document).on('click', '.btn-vote', App.markVoted(chairperson,currentAccount));
-    $(document).on('click', '.btn-winner', App.handleWinner);
-    $(document).on('click', '.btn-register', function(){ var ad = $('#enter_address').val(); App.handleRegister(ad); });
+    $(document).on('click', '#btn-vote', App.markVoted(chairperson,currentAccount));
+    $(document).on('click', '#btn-winner', App.winningProposal);
+    $(document).on('click', '#btnAdd1', function(){ var ad = $('#enter_address').val(); App.addVoter(ad); });
+    $(document).on('click', '#btnAdd2', function(){ var name = $('#candidateName').val(); var id = $('#candidateID').val(); App.addCandidate(name, id); });
+    $(document).on('click', '#vote', function(){ var ad = $('#voterAddress').val(); App.handleVote(ad); });
+    $(document).on('click', '#btnStart', App.startVote);
+    $(document).on('click', '#btnEnd', App.endVote);
+    
+
+  },
+
+  getChairperson : function(){
+    App.contracts.vote.deployed().then(function(instance) {
+      return instance;
+    }).then(function(result) {
+      App.chairPerson = result.constructor.currentProvider.selectedAddress.toString();
+      App.currentAccount = web3.eth.coinbase;
+      if(App.chairPerson != App.currentAccount){
+        jQuery('#panels_new_Ballot').css('display','none');
+        jQuery('#panels_voters').css('display','none');
+        jQuery('#panels_candidate').css('display','none');
+      }else{
+        jQuery('#panels_new_Ballot').css('display','block');
+        jQuery('#panels_voters').css('display','block');
+        jQuery('#panels_candidate').css('display','block');
+      }
+    })
   },
 
 
 
-   populateAddress : function(){ },   //second most
-  
-   getChairperson : function(){ },        //most important
-  
-   handleRegister: function(addr){ },
-  
-   handleVote: function(event){ },
-  
-   handleWinner : function(){ } ,
+  /**
+   * 
+   * @param {*} addr 
+   */
+   addVoter : function(addr){
 
-    markVoted: function(voters , account){            //https://www.trufflesuite.com/tutorials/pet-shop
+        var voteInstance;
+        App.contracts.vote.deployed().then(function(instance) {
+          voteInstance = instance;
+          return voteInstance.register(addr);
+        }).then(function(result, err){
+            if(result){
+                if(parseInt(result.receipt.status) == 1)
+                alert(addr + " registration done successfully")
+                else
+                alert(addr + " registration not done successfully due to revert")
+            } else {
+                alert(addr + " registration failed")
+            }   
+        });
 
-var voteInstance;
-
-App.contracts.vote.deployed().then(function(instance) {  // instance : json 
-
-  voteInstance = instance;
-
-  return voteInstance.getChairperson.call();  // call to getChairPerson , if vote pressed, disable it
-}).then(function(voters) {
-  for (i = 0; i < voters.length; i++) {
-    if (voters[i] !== '0x0000000000000000000000000000000000000000') {
-      $('btn-vote').find('button').text('Success').attr('disabled', true);
-    }
-  }
-}).catch(function(err) {
-  console.log(err.message);
-});
-
-
-};  // end bindEvents
-
-
-
+    },   
   
 
+    /**
+     * 
+     * @param {*} name 
+     * @param {*} id 
+     */
+   addCandidate : function(name, id){
 
-};      //endApp
+
+
+    },   
+   
+
+   /**
+    * 
+    * @param {*} event 
+    */
+   startVote : function(event) {
+
+    $('btnGo').find('button').attr('disabled', true);
+    $('btnStart').find('button').attr('disabled', true);
+    $('btnAdd1').find('button').attr('disabled', true);
+    $('btnAdd2').find('button').attr('disabled', true);
+
+   },
+
+
+   endVote : function(event) {
+
+    $('vote').find('button').attr('disabled', true);
+    $('btnEnd').find('button').attr('disabled', true);
+
+   },
+  
+
+   handleVote: function(ad, event){ },
+  
+   winningProposal : function(){ },
+
+   tallyPoints : function() { },
+
+
+
+
+   /**
+    * 
+    * @param {*} voters 
+    * @param {*} account 
+    */
+   markVoted: function(voters , account){            //https://www.trufflesuite.com/tutorials/pet-shop
+
+    var voteInstance;
+    App.contracts.vote.deployed().then(function(instance) {  // instance : json 
+
+      voteInstance = instance;
+
+      return voteInstance.getChairperson.call();  // call to getChairPerson , if vote pressed, disable it
+      }).then(function(voters) {
+        for (i = 0; i < voters.length; i++) {
+          if (voters[i] !== '0x0000000000000000000000000000000000000000') {
+            $('btn-vote').find('button').text('Success').attr('disabled', true);
+          }
+        }
+      }).catch(function(err) {
+        console.log(err.message);
+
+      });
+   },
+
+
+
+
+   /**
+    * 
+    */
+   populateAddress : function(){
+    new Web3(new Web3.providers.HttpProvider(App.url)).eth.getAccounts((err, accounts) => {
+      jQuery.each(accounts,function(i){
+        if(web3.eth.coinbase != accounts[i]){
+          var optionElement = '<option value="'+accounts[i]+'">'+accounts[i]+'</option';
+          jQuery('#enter_address').append(optionElement);  
+        }
+      });
+    });
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+};      // End of DApp
 
   
 
